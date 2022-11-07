26ce702a26bc74bb9400d33d997860a9fe4e596e
Daniel Felipe Henao Toro <daniel.henao@geniussports.com>
2022-11-06 22:10:34 -0500

    Segunda parte del parcial

--- src/app/coffies/coffie-list/coffie-list.component.ts
+++ src/app/coffies/coffie-list/coffie-list.component.ts
@@ -10,13 +10,27 @@ import { CoffiesService } from '../coffies.service';
 export class CoffieListComponent implements OnInit {
 
   coffies: Array<Coffie> = []
+  totalOrigen: number = 0
+  totalBlend: number = 0
 
   constructor(private coffiesService: CoffiesService) { }
 
   getCoffies(): void {
     this.coffiesService.getCoffies().subscribe(coffies => {
       this.coffies = coffies
+      this.calculateOriginCoffie()
+      this.calculateBlendCoffie()
     })
+  }
+
+  calculateOriginCoffie(): void {
+    const originCoffie = this.coffies.filter(coffie => coffie.tipo === "Café de Origen")
+    this.totalOrigen = originCoffie.length;
+  }
+
+  calculateBlendCoffie(): void {
+    const blendCoffie = this.coffies.filter(coffie => coffie.tipo === "Blend")
+    this.totalBlend = blendCoffie.length;
   }
 
   ngOnInit() {
